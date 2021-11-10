d6ca5456344e8c53026ffb155a6a0722af955c91
Raffaele Montella <raffaele.montella@uniparthenope.it>
2021-11-10 13:13:04 +0100

    Updated and moved

--- src/it/uniparthenope/informatica/prgriii3/Main.java
+++
@@ -1,8 +0,0 @@
-package it.uniparthenope.informatica.prgriii3;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ src/it/uniparthenope/informatica/prgriii9/l00/HelloWorld.java
@@ -0,0 +1,9 @@
+package it.uniparthenope.informatica.prgriii9.l00;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Java World!");
+    }
+}
